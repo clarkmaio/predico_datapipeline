0e63f1eeaac9e544dd4caaf8786fb58cf0c0e384
clarkmaio <maioliandrea0@gmail.com>
2024-12-16 22:52:52 +0100

    ecmwf loader and workflows

--- src/pipeline/ecmwf.py
+++ src/pipeline/ecmwf.py
@@ -13,15 +13,19 @@ from copy import deepcopy
 import s3fs
 from loguru import logger
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from src.utils import upload_dataframe_hf
 
-MAX_STEP = 51
+MAX_STEP = 6 #51
 ONSHORE_BOX={'latitude': (51.5, 49.5), 'longitude': (2.5, 6.5)}
 OFFSHORE_BOX={'latitude': (), 'longitude': ()}
 
 def uri_generator(forecastdate: datetime, run: str, step: int) -> str:
-    step_str = str(step).zfill(3)
-    return f'simplecache::s3://noaa-gefs-pds/gefs.{forecastdate.strftime("%Y%m%d")}/{run}/atmos/pgrb2sp25/geavg.t{run}z.pgrb2s.0p25.f{step_str}'
+    frun_str = forecastdate.strftime("%Y%m%d")
+    full_frun_str = f'{frun_str}{run}0000'
+    return f'simplecache::s3://ecmwf-forecasts/{frun_str}/{run}z/ifs/0p25/oper/{full_frun_str}-{step}h-oper-fc.grib2'
 
 
 def build_dataset(uri: str) -> pd.DataFrame:
@@ -34,20 +38,18 @@ def build_dataset(uri: str) -> pd.DataFrame:
                      )
     m10 = m10.sel(latitude=slice(51.5, 49.5), longitude=slice(2.5, 6.5)).to_dataframe()
     m10.reset_index(inplace=True)
+    m10.drop('heightAboveGround', axis=1, errors='ignore', inplace=True)
 
-
-    # Surface variables
-    surface = xr.open_dataset(file, 
+    # 100m variables
+    m100 = xr.open_dataset(file, 
                      engine="cfgrib", 
-                     filter_by_keys={'typeOfLevel': 'surface'}
+                     filter_by_keys={'typeOfLevel': 'heightAboveGround', 'level': 100}
                      )
+    m100 = m100.sel(latitude=slice(51.5, 49.5), longitude=slice(2.5, 6.5)).to_dataframe()
+    m100.reset_index(inplace=True)
+    m100.drop('heightAboveGround', axis=1, errors='ignore', inplace=True)
 
-    surface = surface.sel(latitude=slice(51.5, 49.5), longitude=slice(2.5, 6.5))
-    surface = surface[['gust', 'cape']].to_dataframe()
-    surface.reset_index(inplace=True)
-
-    data = pd.merge(m10, surface, on=['latitude', 'longitude', 'valid_time', 'step', 'time'])
-    data.drop(['heightAboveGround', 'surface'], axis=1, inplace=True)
+    data = pd.merge(m10, m100, on=['latitude', 'longitude', 'valid_time', 'step', 'time'])
 
     os.remove(file)
     return data
@@ -67,27 +69,28 @@ def download_forecastrun(date: datetime, run: str, steps: List[int]) -> pd.DataF
             tmp = build_dataset(uri=uri)
             run_data.append(tmp)
         except Exception as e:
+            logger.warning(e)
             continue
 
     run_data = pd.concat(run_data, axis=0)
     return run_data
 
 
-def deduce_gfs_lastrun() -> Tuple:
+def deduce_ecmwf_lastrun() -> Tuple:
     '''
     Return last forecastdate and run available on aws
     '''
     fs = s3fs.S3FileSystem(anon=True)
-    folders = fs.ls('s3://noaa-gefs-pds/')
+    folders = fs.ls('s3://ecmwf-forecasts/')
     folders.sort()
 
-    lastrundate_path=folders[-2]
-    lastrundate = datetime.strptime(lastrundate_path.split('/')[-1].replace('gefs.', ''), '%Y%m%d')
+    lastrundate_path=folders[-4]
+    lastrundate = datetime.strptime(lastrundate_path.split('/')[-1], '%Y%m%d')
     lastrun_path = fs.ls(f's3://{lastrundate_path}')[-1]
-    lastrun = lastrun_path.split('/')[-1]
+    lastrun = lastrun_path.split('/')[-1].replace('z', '')
     return lastrundate, lastrun
 
-def GfsHistoryPipeline(date_range: Iterable, runs: List[str] = ['00', '06'], steps: List[int] = list(range(0, MAX_STEP, 3))):
+def EcmwfHistoryPipeline(date_range: Iterable, runs: List[str] = ['00', '06'], steps: List[int] = list(range(0, MAX_STEP, 3))):
     '''
     Download historical weather data from aws
     '''
@@ -102,30 +105,30 @@ def GfsHistoryPipeline(date_range: Iterable, runs: List[str] = ['00', '06'], ste
     for (d, run) in pbar: 
         pbar.set_postfix_str(f'{d} {run}')  
         run_data = download_forecastrun(date=d, run=run, steps=steps)
-        upload_dataframe_hf(df=run_data, filename='gfs_history.parquet', concat=True)
+        upload_dataframe_hf(df=run_data, filename='ecmwf_history.parquet', concat=True)
 
 
-def GfsLastrunPipeline():
+def EcmwfLastrunPipeline():
     '''
-    Download last run available from gfs bucket and upload on huggingface
+    Download last run available from ecmwf bucket and upload on huggingface
     '''
     steps = list(range(0, MAX_STEP, 3))
 
-    lastrundate, lastrun = deduce_gfs_lastrun()
+    lastrundate, lastrun = deduce_ecmwf_lastrun()
     logger.info(f'Download run {lastrundate.strftime("%Y-%m-%d")} {lastrun}z')
     run_data = download_forecastrun(date=lastrundate, run=lastrun, steps=steps)
 
     login(token=os.getenv('HF_TOKEN'), write_permission=True)
 
-    logger.info('Write gfs_lastrun.parquet')
-    upload_dataframe_hf(df=run_data, filename='gfs_lastrun.parquet', concat=False)
+    logger.info('Write ecmwf_lastrun.parquet')
+    upload_dataframe_hf(df=run_data, filename='ecmwf_lastrun.parquet', concat=False)
 
-    logger.info('Update gfs_history.parquet')
-    upload_dataframe_hf(df=run_data, filename='gfs_history.parquet', concat=True)
+    logger.info('Update ecmwf_history.parquet')
+    upload_dataframe_hf(df=run_data, filename='ecmwf_history.parquet', concat=True)
 
     logger.info('Done')
 
 
 
 if __name__ == '__main__':
-    GfsLastrunPipeline()
+    EcmwfLastrunPipeline()
